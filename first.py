ef76af14510b025c6111eb676d1e481b1ee6ffce
Meda Ruthu <51402756+Ruthu417@users.noreply.github.com>
2025-03-07 14:45:42 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,11 @@
+print('
+welcome to r+ mode') for dig in range(1,n//2+1):
+        if n%dig==0:
+            fc+=1
+    if fc==1:
+        return True
+    else:
+        return False
+    
+    
+#def is_prime(n)
